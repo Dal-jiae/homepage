f9757868fa7b88c22ff45e08fcbb59504ea3b516
Dal-jiae <moon991110@gmail.com>
2025-07-31 16:18:05 +0900

    네비바 형태 고정

--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -5,8 +5,8 @@ function Header ({setShowList, showList}) {
 
   return(
     <>
-    <nav class="navbar navbar-expand-lg bg-body-tertiary">
-      <div class="container-fluid" style={{
+    <nav className="navbar bg-body-tertiary">
+      <div className="container-fluid" style={{
         fontSize: '1.2rem',
         display: 'flex',
         alignItems: 'center',
@@ -19,14 +19,14 @@ function Header ({setShowList, showList}) {
             paddingLeft: '20px',
           }}>
             미술사학연구회</a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
         </button>
-        <div class="collapse navbar-collapse" id="navbarNav" style={{
+        <div className="collapse navbar-collapse" id="navbarNav" style={{
           display: 'flex',
-          justifyContent: 'pace-between'
+          justifyContent: 'space-between'
         }}>
-          <ul class="navbar-nav" onClick={()=> setShowList(!showList)} 
+          <ul className="navbar-nav" onClick={()=> setShowList(!showList)} 
           style={{ 
               display: 'flex',
               gap: '7.5rem', /* 항목 사이 간격, 원하는 값으로 조정 */
@@ -35,17 +35,17 @@ function Header ({setShowList, showList}) {
               listStyle: 'none',
               paddingLeft: '7rem'
           }}>
-            <li class="nav-item">
-              <a class="nav-link" href="#" >학회 안내</a>
+            <li className="nav-item">
+              <a className="nav-link" href="#" >학회 안내</a>
             </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#" >학술지</a>
+            <li className="nav-item">
+              <a className="nav-link" href="#" >학술지</a>
             </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#" >논문투고</a>
+            <li className="nav-item">
+              <a className="nav-link" href="#" >논문투고</a>
             </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#" >학회소식</a>
+            <li className="nav-item">
+              <a className="nav-link" href="#" >학회소식</a>
             </li>
           </ul>
         </div>
