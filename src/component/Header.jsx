454d25bd2b90fa6a76d0b903222081766498556e
Dal-jiae <moon991110@gmail.com>
2025-08-01 11:03:15 +0900

    이미지슬라이더 반응형

--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -32,19 +32,22 @@ function Header ({setShowList, showList}) {
           style={{ 
               display: 'flex',
               flexDirection: 'row',
-              gap: '7.5rem', /* 항목 사이 간격, 원하는 값으로 조정 */
+              // gap: '7.5rem', /* 항목 사이 간격, 원하는 값으로 조정 */
               padding: '0',
               margin: '0',
               listStyle: 'none',
               paddingLeft: '7rem'
           }}>
-            <li className="nav-item">
+            <li className="nav-item" 
+            style={{marginRight: '7.5rem'}}>
               <a className="nav-link" href="#" >학회 안내</a>
             </li>
-            <li className="nav-item">
+            <li className="nav-item"
+            style={{marginRight: '7.5rem'}}>
               <a className="nav-link" href="#" >학술지</a>
             </li>
-            <li className="nav-item">
+            <li className="nav-item"
+            style={{marginRight: '7.5rem'}}>
               <a className="nav-link" href="#" >논문투고</a>
             </li>
             <li className="nav-item">
